15502d604f480a453856feaff489fb965281d279
Jaynel <lenyajpatiarba@gmail.com>
2023-12-11 23:19:48 +0800

    seperating empty component

--- ProjectGROCERYbai/Components/Container/WeeklyPlanner/Planner.js
+++ ProjectGROCERYbai/Components/Container/WeeklyPlanner/Planner.js
@@ -1,10 +1,13 @@
 
-import { View, TextInput, TouchableOpacity, KeyboardAvoidingView, StyleSheet, Image, Alert } from 'react-native'
+import { View, TextInput, TouchableOpacity, KeyboardAvoidingView, StyleSheet, Image, Alert, ScrollView } from 'react-native'
 
 import { Text,Icon, Button } from 'react-native-paper'
 
 import WeekContainer from './WeekContainer'
 import { useState } from 'react'
+
+
+import Empty from './Empty'
 
 export default function Planner() {
      const [list, setList] = useState([
@@ -20,25 +23,9 @@ export default function Planner() {
 
      const [list1, setList1] = useState([])
 
-     if (list.length == 0) {
+     if (list.length == 2) {
           return (
-               <KeyboardAvoidingView style={style.container}>
-                    <View>
-                         <View style={{alignItems: 'center'}}>
-                              <Image source={require('../../../assets/gorcerybai.png')} style={{width: 200, height: 200}} />
-                         </View>
-                         <View style={{alignItems: 'center'}}>
-                              <Text variant='titleMedium'>No Weekly Planner Added</Text>
-                              <Text variant='labelLarge'>Hit the Green button down below to Create a Weekly Planner</Text>
-                         </View>
-
-                         <View style={{marginTop: 250, alignItems: 'center'}}>
-                              <Button mode='contained-tonal' buttonColor='#18B127' textColor='white' icon={"clipboard-list-outline"}
-                              onPress={() => (Alert.alert('You have click this button', 'nice'))} style={{width: 314, height:70, justifyContent: 'center'}}>Add New Weekly Planner</Button>
-                         </View>
-                    </View>
-                    
-               </KeyboardAvoidingView>
+               <Empty title="No Weekly Planner Added" label="Hit the Green button down below to Create a Weekly Planner" buttonTitle="Add New Weekly Planner"/>
           )
      }
      return (
@@ -47,16 +34,14 @@ export default function Planner() {
                     <View style={{alignItems: 'center', bottom: 450, left: 35, position: 'absolute'}}>
                          <Text variant='titleLarge' style={{fontWeight: 'bold'}}>Grocery Weekly Planner</Text>
                     </View>
-
-                    <View>
-
-                         {
-                              list.map((list, index) => {
-                                   return <WeekContainer week={list.price} total={list.price} key={index}/>
-                              })
-                         }
-                    </View>
-
+                    
+                         <View>
+                              {
+                                   list.map((list, index) => {
+                                        return <WeekContainer week={list.price} total={list.price} key={index}/>
+                                   })
+                              }
+                         </View>
                     <View>
                          <Button mode='contained-tonal' buttonColor='#18B127' textColor='white' icon={"clipboard-list-outline"}
                          onPress={() => (Alert.alert('You have click this button', 'nice'))}>Add New Weekly Planner</Button>
