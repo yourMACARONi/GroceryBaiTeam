bb53d2c93abdad166d2ebe404d074cfa45bdf9d6
Jaynel <lenyajpatiarba@gmail.com>
2023-12-11 13:42:14 +0800

    Adding profile component

--- ProjectGROCERYbai/App.js
+++ ProjectGROCERYbai/App.js
@@ -37,7 +37,7 @@ export default function App() {
 
             <Stack.Screen 
               name = "Login"
-              component ={Login}
+              component ={Login1}
               options = {{
                 headerShown: false
               }}
--- ProjectGROCERYbai/Components/Profile1.js
+++ ProjectGROCERYbai/Components/Profile1.js
@@ -1,53 +1,91 @@
-import { View, KeyboardAvoidingView, StyleSheet, ActivityIndicator} from "react-native";
+import { View, KeyboardAvoidingView, StyleSheet, ActivityIndicator, Alert, Image, TouchableOpacity} from "react-native";
 
 import { TextInput, Text } from "react-native-paper";
 
 import * as SecureStore from 'expo-secure-store'
 
 import URL from '../api/constants'
-import { useEffect } from "react";
-import { useQuery, QueryClient } from "react-query";
+import { useEffect, useState } from "react";
 
-async function userMe() {
-     const response = await fetch(`${URL}/api/user/me`, {
-          headers: {
-               'Authorization': `Bearer ${ await SecureStore.getItemAsync('token')}`
+
+
+export default function Profile1( { navigation }) {
+     const [user, setUser] = useState([]);
+
+     useEffect(() => {
+          async function userMe() {
+               console.log( await SecureStore.getItemAsync('token'))
+               const response = await fetch(`${URL}/api/user/me`, {
+                    headers: {
+                         'Authorization': `Bearer ${ await SecureStore.getItemAsync('token')}`
+                    }
+               })
+
+               if (response.status == 403) {navigation.replace('Login'); return Alert.alert("Token has been expired", "Please Login Again")}
+
+               const message =  await response.json()
+
+               setUser(message)
           }
-     })
 
-     const message = await response.json()
-
-     return message
-}
-
-export default function Profile1() {
-
-     const { isLoading, error, data } = useQuery(["profile"], userMe,
-     {
-          staleTime: 3000
-     })
-
+          userMe()
+     }, [])
 
      return (
      <KeyboardAvoidingView style={style.container}>
           <View>
                <View style={{flex: 1, margin: 100, alignItems: 'center'}}>
                     <View style={{marginBottom: 100}}>
-                         <Text variant="titleLarge">Personal details</Text>
+                         <Text variant="titleLarge" style={{fontWeight: 'bold'}}>Personal details</Text>
                     </View>
 
-                    <View>
-                         {
-                              data.map((data, index) => {
-                                   return (
-                                   <View>
-                                        <Text key={index}>{data.name}</Text>
-                                        <Text key={index}>{data.email}</Text>
-                                   </View>
-                                   )
+                    <View style={{alignItems: 'center'}}>
+                         <TouchableOpacity style={{marginBottom: 50}}>
+                              <View style={{marginBottom: 10, flexDirection: 'row', justifyContent: 'space-between'}}>
+                                   <Text variant="titleSmall">Personal Information</Text>
+                                   <Text variant="titleSmall" style={{color: 'green'}}>change</Text>
+                              </View>
 
-                              })
-                         }
+                              <View style={{borderColor: 'black', backgroundColor: 'white', width: 400, height: 200, borderRadius: 20, flexDirection:'row', shadowColor: "#000",shadowOffset: {width: 0,height: 1,},shadowOpacity: 0.22,shadowRadius: 2.22,elevation: 3,}}>
+                                   <View style={{ backgroundColor:'white', borderRadius: 10, alignItems:'center', height: 100, marginTop: 50, marginLeft: 50}}>
+                                        <Image source={require('../assets/google.png')} style={{ height: 100, width: 100}}></Image>
+                                   </View>
+
+                                   <View style={{margin: 50, marginTop: 70}}>
+                                        <Text variant='titleMedium'>{`${user.first_name} ${user.last_name}`}</Text>
+                                        <Text variant='titleSmall'>{user.email}</Text>
+                                   </View>
+
+                              </View>
+                         </TouchableOpacity>
+                    
+                         <View>
+                              <View style={{marginBottom: 20, backgroundColor: 'white', height: 50, width: 400, borderRadius: 20, justifyContent:'center', shadowColor: "#000",shadowOffset: {width: 0,height: 1,},shadowOpacity: 0.22,shadowRadius: 2.22,elevation: 3,}}>
+                                   <TouchableOpacity>
+                                        <Text style={{marginLeft: 30}} variant='titleMedium'>Added Items</Text>
+                                   </TouchableOpacity>
+                              </View>
+
+                              <View style={{marginBottom: 20, backgroundColor: 'white', height: 50, width: 400, borderRadius: 20, justifyContent:'center', shadowColor: "#000",shadowOffset: {width: 0,height: 1,},shadowOpacity: 0.22,shadowRadius: 2.22,elevation: 3,}}>
+                                   <TouchableOpacity>
+                                        <Text style={{marginLeft: 30}} variant='titleMedium'>Password Reset</Text>
+                                   </TouchableOpacity>
+                              </View>
+
+                              <View style={{marginBottom: 20, backgroundColor: 'white', height: 50, width: 400, borderRadius: 20, justifyContent:'center', shadowColor: "#000",shadowOffset: {width: 0,height: 1,},shadowOpacity: 0.22,shadowRadius: 2.22,elevation: 3,}}>
+                                   <TouchableOpacity>
+                                        <Text style={{marginLeft: 30}} variant='titleMedium'>FAQ</Text>
+                                   </TouchableOpacity>
+                              </View>
+
+
+                              <View style={{marginBottom: 20, backgroundColor: 'white', height: 50, width: 400, borderRadius: 20, justifyContent:'center', shadowColor: "#000",shadowOffset: {width: 0,height: 1,},shadowOpacity: 0.22,shadowRadius: 2.22,elevation: 3,}}>
+                                   <TouchableOpacity>
+                                        <Text style={{marginLeft: 30, color:'red'}} variant='titleMedium'>Logout</Text>
+                                   </TouchableOpacity>
+                              </View>
+                         </View>
+
                     </View>
                </View>
           </View>
